8ffadc3efd3f648eb8cdd01f1060590f92cf71b8
Diego Cristian Alfredo Veras <verasdiegoca78@gmail.com>
2023-03-25 20:52:42 -0300

    Update Ej1.java

--- ej1/src/ej1/Ej1.java
+++ ej1/src/ej1/Ej1.java
@@ -3,7 +3,11 @@ package ej1;
 public class Ej1 {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo1");
+        System.out.println("Hola mundo2");
+        System.out.println("Hola mundo3");
+        System.out.println("Hola mundo4");
+        System.out.println("Hola mundo5");
 
     }
 
